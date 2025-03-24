676324873146995ecdc916102aa14d83f1c6803f
larissaboingg <larissa_boing@estudante.sesisenai.org.br>
2025-03-23 22:57:58 -0300

    Add - tudo

--- script.js
+++ script.js
@@ -50,7 +50,43 @@ function verificarTamanho (tamanho){
 verificarTamanho (tamanho)
 
 //Atividade 16
-const fruta = [banana,maca,caqui,melancia,kiwi];
+ let x = 0;
+     while (x <= 4) {
+     let fruta = prompt ("Escreva uma fruta:");
+     x = x + 1;
+     if (x==3) {
+         console.log(fruta)
+     }
+ }
+
+//Atividade 17
+let sauda = "Bem-vindo(a) ";
+let nome = prompt ("Escreva seu nome:");
+function saudacao (sauda,nome){
+    console.log(sauda + nome);
+}
+saudacao(sauda,nome)
+
+
+//Atividade 18
+let nota = prompt ("Digite sua nota:");
+function verificarAprovacao (nota) {
+    if (nota>=6) {
+        console.log ("Aprovado")
+    } else {
+        console.log ("Reprovado")
+    }
+}
+verificarAprovacao(nota)
+
+//Atividade 19
+let numer = 1 ;
+while (numer<=30){
+    numer++;
+    if (numer % 2 == 0){
+        console.log (numer);
+    }
+}
 
 //Atividade 20 
 let palavra = prompt ("Digite uma palavra:")
@@ -65,3 +101,53 @@ function contarVogais (palavra) {
 }
 console.log(contador);
 contarVogais (palavra)
+
+//Atividade 21
+let array = [2, 5, 9, 6, 3];
+console.log(array[0] + array[1] + array[2] + array[3] + array[4])
+
+
+// atividade 22
+let n = prompt("Digite um número:")
+function dobrarNumeros(n){
+    console.log(parseInt(n) + parseInt(n))
+}
+dobrarNumeros(n)
+
+
+//Atividade 22
+let nume = prompt ("Digite um numero:");
+function dobrarNumeros (nume) {
+    console.log ("O dobro desse numero é: "+ nume*nume)
+}
+dobrarNumeros (nume)
+
+
+//Atividade 23
+let num = 2 ;
+while (num<=20){
+    num++;
+    if (num % 2 == 0){
+        console.log (num);
+    }
+}
+
+
+//Atividade 24
+let maiuscula = prompt("Digite a palavra:")
+function capitalizar(maiuscula){
+    console.log(maiuscula.charAt(0).toUpperCase() + maiuscula.slice(1));
+}
+capitalizar(maiuscula)
+
+
+// atividade 25
+let ano = prompt("Digite um ano:")
+function anoBissexto(ano){
+    if (ano % 4 === 0){
+        console.log("É um ano bissexto")
+    }else {
+        console.log("Não é um ano bissexto")
+    }
+}
+anoBissexto(ano)
